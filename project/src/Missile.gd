9824fecf0f9c555526b0739e2a954ebbc712079a
CaKellum <cakellum@bsu.edu>
2020-11-08 15:00:48 -0500

    Adds Player movement working, missiles broke

--- project/src/Missile.gd
+++ project/src/Missile.gd
@@ -2,15 +2,18 @@ class_name Missile
 extends KinematicBody2D
 
 
-const SPEED := 10
+const SPEED := 1
 var _velocity :=  Vector2()
 
 func fired(pos:Vector2)->void:
 	self.position = pos
 	_velocity = Vector2(0,SPEED)
+	apply_scale(_velocity)
 
-func _physics_process(delta:float):
-	var collision := move_and_collide(_velocity*delta)
+
+func _process(_delta:float)->void:
+	print(_velocity)
+	var collision := move_and_collide(_velocity)
 	if collision:
 		if collision.colider.has_method("hit"):
 			collision.colider.hit()
--- project/src/PlayerShip.gd
+++ project/src/PlayerShip.gd
@@ -2,7 +2,11 @@ extends KinematicBody2D
 
 signal game_over
 
+const RIGHT_MUZZEL = Vector2(15,13)
+const LEFT_MUZZEL = Vector2(-15,13)
+
 var _velocity := Vector2.ZERO
+var amt_fired_missiles := 0
 
 export var _speed := 100
 
@@ -18,16 +22,23 @@ func _get_input()->void:
 		velocity.x += 1
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
-		
+	
 	_velocity = velocity.normalized() * _speed
+	position.x = clamp(position.x, 0, 480)
 	
 	if Input.is_action_pressed("fire"):
 		_fire_missle()
 
 
 func _fire_missle():
-	var shot = Missile.instance()
-	shot.fired(Vector2(position.x,position.y+10))
+	var muzzel := Vector2()
+	var shot := Missile.new()
+	amt_fired_missiles +=1
+	if amt_fired_missiles%2 == 0:
+		muzzel = RIGHT_MUZZEL
+	else:
+		muzzel = LEFT_MUZZEL
+	shot.fired(muzzel)
 
 
 func hit()->void:
@@ -37,6 +48,7 @@ func hit()->void:
 		emit_signal("game_over")
 	else:
 		player_health.loose_life()
+
 
 func _physics_process(_delta:float)->void:
 	_get_input()
